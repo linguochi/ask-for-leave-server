71495599280c379818a1fab8ec9b4766dd4bd109
linguochi <382150914@qq.com>
2018-01-16 16:02:31 +0800

    add 配置信息

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const app = new Koa();
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
-app.listen(3000);
