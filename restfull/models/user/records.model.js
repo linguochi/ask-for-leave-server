d49c4215e72b525e37a9806154af76cc6191256b
linguochi <382150914@qq.com>
2018-01-17 10:57:21 +0800

    表设计

---
+++ restfull/models/user/records.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecordSchema = new Schema({
+  from: {}, // 从什么时候开始请假
+  to: {}, // 请到什么时候
+  user_id: {}, // 谁请假
+  apply_user_id: {},//向谁请假
+  description: {},//为什么请假
+  status: {}, //状态码 {0：审批中 1：同意请假 2：请假不通过 3：取消请假}
+});
+
+module.exports = mongoose.model('Record', RecordSchema);
--- restfull/models/user/user.model.js
+++ restfull/models/user/user.model.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true }, // 用户名
   nickname: { type: String, required: true }, // 昵称
-  phoneNumber: { type: String, default: '' },
+  phoneNumber: { type: String, default: '' }, // 手机号
   avatar: { type: String, default: '' },    // 头像
+  openId: { type: String, default: '', required: true },
+  sex: { type: Number, default: 0 }, //性别
 });
 
 module.exports = mongoose.model('User', UserSchema);
