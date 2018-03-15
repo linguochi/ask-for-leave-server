7be91741e30a7237d1c9ee534bfdd40545d48e2b
linguochi <382150914@qq.com>
2018-03-15 11:02:32 +0800

    修改用户表

--- restfull/models/user/user.model.js
+++ restfull/models/user/user.model.js
@@ -2,17 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: { type: String, required: true }, // 用户名
-  nickname: { type: String, required: true }, // 昵称
-  phoneNumber: { type: String, default: '' }, // 手机号
-  avatar: { type: String, default: '' },    // 头像
+  name: {type: String, required: true}, // 用户名
+  phoneNumber: {type: String, default: ''}, // 手机号
+  password: String, //密码
   leader: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     default: '5a781f7ad0e48801c759f88e',
   },
-  openId: { type: String, default: '', required: true },
-  sex: { type: Number, default: 0 }, //性别
+  openId: String,
 });
 
 module.exports = mongoose.model('User', UserSchema);
